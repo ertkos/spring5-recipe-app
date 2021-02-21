a27e48bff2190ed156f2d0181eac1ebf216b0286
ert112233 <ertugrlkose@gmail.com>
2021-02-21 08:46:27 +0300

    ok

--- src/test/java/guru/springframework/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -8,6 +8,7 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
@@ -20,9 +21,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-/**
- * Created by jt on 6/17/17.
- */
 public class IndexControllerTest {
 
     @Mock
@@ -31,50 +29,44 @@ public class IndexControllerTest {
     @Mock
     Model model;
 
-    IndexController controller;
+    IndexController indexController;
+
 
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
-
-        controller = new IndexController(recipeService);
+        indexController = new IndexController(recipeService);
     }
 
     @Test
     public void testMockMVC() throws Exception {
-        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
-
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("index"));
     }
 
     @Test
-    public void getIndexPage() throws Exception {
-
-        //given
-        Set<Recipe> recipes = new HashSet<>();
-        recipes.add(new Recipe());
-
-        Recipe recipe = new Recipe();
-        recipe.setId(1L);
-
-        recipes.add(recipe);
-
-        when(recipeService.getRecipes()).thenReturn(recipes);
+    public void getIndexPage() {
+        Set<Recipe> recipeData = new HashSet<>();
+        Recipe recipe1 = new Recipe();
+        recipe1.setId(1L);
+        Recipe recipe2 = new Recipe();
+        recipe2.setId(2L);
+        recipeData.add(recipe1);
+        recipeData.add(recipe2);
+        when(recipeService.getRecipes()).thenReturn(recipeData);
 
         ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
 
-        //when
-        String viewName = controller.getIndexPage(model);
 
+        assertEquals("index", indexController.getIndexPage(model));
 
-        //then
-        assertEquals("index", viewName);
         verify(recipeService, times(1)).getRecipes();
         verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+
         Set<Recipe> setInController = argumentCaptor.getValue();
         assertEquals(2, setInController.size());
-    }
 
+    }
 }
--- src/test/java/guru/springframework/services/RecipeServiceImplTest.java
+++ src/test/java/guru/springframework/services/RecipeServiceImplTest.java
@@ -1,6 +1,5 @@
 package guru.springframework.services;
 
-
 import guru.springframework.domain.Recipe;
 import guru.springframework.repositories.RecipeRepository;
 import org.junit.Before;
@@ -14,9 +13,6 @@ import java.util.Set;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
-/**
- * Created by jt on 6/17/17.
- */
 public class RecipeServiceImplTest {
 
     RecipeServiceImpl recipeService;
@@ -24,27 +20,22 @@ public class RecipeServiceImplTest {
     @Mock
     RecipeRepository recipeRepository;
 
-
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
-
         recipeService = new RecipeServiceImpl(recipeRepository);
     }
 
     @Test
-    public void getRecipes() throws Exception {
-
+    public void getRecipes() {
         Recipe recipe = new Recipe();
-        HashSet receipesData = new HashSet();
-        receipesData.add(recipe);
-
-        when(recipeService.getRecipes()).thenReturn(receipesData);
+        Set<Recipe> recipeData = new HashSet<>();
+        recipeData.add(recipe);
+        when(recipeRepository.findAll()).thenReturn(recipeData);
 
         Set<Recipe> recipes = recipeService.getRecipes();
+        assertEquals(1,recipes.size());
 
-        assertEquals(recipes.size(), 1);
-        verify(recipeRepository, times(1)).findAll();
+        verify(recipeRepository, times(1));
     }
-
 }
